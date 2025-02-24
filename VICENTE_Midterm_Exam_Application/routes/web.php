2cc93fc9e2bb404bdbc4cf5a10783d4f42c93d2c
vicente-jsp <jpv0708@students.uc-bcf.edu.ph>
2025-02-24 14:15:08 +0000

    Finalized display of restaurants in welcome.blade.php

--- VICENTE_Midterm_Exam_Application/routes/web.php
+++ VICENTE_Midterm_Exam_Application/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
